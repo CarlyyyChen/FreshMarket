7f365493aaba2a96f5cc3b304ff6ca46247f3c6c
ZachYu-1 <zhekunyu9@gmail.com>
2023-12-02 17:04:00 -0500

    push muti funcs on to main branch

--- Web/app.py
+++ Web/app.py
@@ -1,8 +1,16 @@
-from flask import Flask, request, redirect, url_for, render_template
+from flask import Flask, request, redirect, url_for, render_template, jsonify
 import pymysql.cursors
-import time
+import datetime
 
 app = Flask(__name__)
+
+db_config = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': '123456',
+    'database': 'fresh_market_db',
+    'cursorclass': pymysql.cursors.DictCursor
+}
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -14,7 +22,7 @@ def register():
         conn = pymysql.connect(host='localhost',
                                user='root',
                                password='123456',
-                               database='test_proj',
+                               database='fresh_market_db',
                                cursorclass=pymysql.cursors.Cursor)
         with conn:
             with conn.cursor() as cursor:
@@ -34,9 +42,185 @@ def register():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        email = request.form.get('username')
+        username = request.form.get('username')
         password = request.form.get('password')
-        return render_template('login.html', error_message='Success')
+        conn = pymysql.connect(host='localhost',
+                               user='root',
+                               password='123456',
+                               database='fresh_market_db',
+                               cursorclass=pymysql.cursors.Cursor)
+        cursor = conn.cursor()
+        sql = "call login(\'" + username + "\', \'" + password + "\')"
+        try:
+            cursor.execute(sql)
+            result = cursor.fetchone()
+            conn.commit()
+            return render_template('login.html', error_message=result[0])
+        except pymysql.err.OperationalError as e:
+            error_code, message = e.args
+            return render_template('login.html', error_message=message)
+    else:
+        return render_template('login.html')
+
+
+@app.route('/neworder')
+def neworder():
+    return render_template('neworder.html')
+
+
+@app.route('/get_store_id')
+def get_store_id():
+    conn = pymysql.connect(**db_config)
+    cursor = conn.cursor()
+    query = "SELECT store_id FROM store"
+    cursor.execute(query)
+    store_id = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return jsonify(store_id)
+
+
+@app.route('/get_customer_id')
+def get_customer_id():
+    conn = pymysql.connect(**db_config)
+    cursor = conn.cursor()
+    query = "SELECT customer_id FROM customer"
+    cursor.execute(query)
+    customer_id = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return jsonify(customer_id)
+
+
+@app.route('/get_supplier_id')
+def get_supplier_id():
+    conn = pymysql.connect(**db_config)
+    cursor = conn.cursor()
+    query = "SELECT supplier_id FROM supplier"
+    cursor.execute(query)
+    supplier_id = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return jsonify(supplier_id)
+
+
+@app.route('/get_product_types')
+def get_product_types():
+    conn = pymysql.connect(**db_config)
+    cursor = conn.cursor()
+    query = "SELECT type_id, name FROM product_type"
+    cursor.execute(query)
+    product_types = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return jsonify(product_types)
+
+
+@app.route('/get_product_names/<int:type_id>')
+def get_product_names(type_id):
+    conn = pymysql.connect(**db_config)
+    cursor = conn.cursor()
+    query = f"SELECT product_id, name FROM product WHERE type_id = {type_id}"
+    cursor.execute(query)
+    product_names = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return jsonify(product_names)
+
+
+@app.route('/submit_order', methods=['POST'])
+def submit_order():
+    try:
+        now_date = datetime.date.today()
+        # Access the JSON data sent with the POST request
+        data = request.json
+        print(data)
+        # Insert the order details into the "order_detail" table
+        conn = pymysql.connect(**db_config)
+        cursor = conn.cursor()
+        # query = "INSERT INTO order_detail (product_type_id, product_id) VALUES (%s, %s)"
+        query = "call new_order(%s, %s, %s, %s, %s, %s, @message)"
+        for line in data['lines']:
+            cursor.execute(query, (line['order_id'], now_date, line['customer_id'], line['store_id'],
+                                   line['product_name'], line['quantity']))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return jsonify({'success': True, 'message': 'Order submitted successfully'})
+    except pymysql.err.IntegrityError as e:
+        return jsonify({'success': False, 'message': str(e)})
+
+
+@app.route('/delivery')
+def delivery():
+    return render_template('delivery.html')
+
+
+@app.route('/get_order_id')
+def get_order_id():
+    conn = pymysql.connect(**db_config)
+    cursor = conn.cursor()
+    query = "SELECT order_id FROM orders WHERE status = \'filled\'"
+    cursor.execute(query)
+    order_id = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return jsonify(order_id)
+
+
+@app.route('/get_truck_id')
+def get_truck_id():
+    conn = pymysql.connect(**db_config)
+    cursor = conn.cursor()
+    query = "SELECT license_plate FROM freight_truck WHERE availability = true"
+    cursor.execute(query)
+    order_id = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return jsonify(order_id)
+
+
+@app.route('/assign_delivery', methods=['POST'])
+def assign_delivery():
+    try:
+        # Access the JSON data sent with the POST request
+        data = request.json
+        conn = pymysql.connect(**db_config)
+        cursor = conn.cursor()
+        query = "call assign_delivery(%s, %s)"
+        for line in data['lines']:
+            cursor.execute(query, (line['truck_id'], line['order_id']))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return jsonify({'success': True, 'message': 'Successfully assigned delivery'})
+    except pymysql.err.IntegrityError as e:
+        return jsonify({'success': False, 'message': str(e)})
+
+
+@app.route('/restock')
+def restock():
+    return render_template('restock.html')
+
+
+@app.route('/order_from_supplier', methods=['POST'])
+def order_from_supplier():
+    try:
+        # Access the JSON data sent with the POST request
+        data = request.json
+        conn = pymysql.connect(**db_config)
+        cursor = conn.cursor()
+        query = "call order_from_supplier(%s, %s, %s, %s, %s)"
+        for line in data['lines']:
+            cursor.execute(query, (line['supplier_id'], line['store_id'], line['prod_name'], line['quantity'],
+                                   line['prod_type']))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return jsonify({'success': True, 'message': 'Successfully ordered product from supplier'})
+    except pymysql.err.OperationalError as e:
+        error_code, message = e.args
+        return jsonify({'success': False, 'message': message})
 
 
 if __name__ == '__main__':
