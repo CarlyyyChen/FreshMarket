431043db603e348d7363cdaae7c7eac49abb0dd0
CarlyyyChen <huanyuechen@outlook.com>
2023-12-04 17:56:59 -0500

    update promotion procedure

--- FreshMarketProdecures.sql
+++ FreshMarketProdecures.sql
@@ -277,6 +277,7 @@ CREATE PROCEDURE launch_promotion(
     IN inputDiscount DECIMAL(5, 2)
 )
 BEGIN
+	IF CURDATE() >= inputStartDate AND CURDATE() <= inputEndDate THEN
     -- Add new row to promotion table
     INSERT INTO promotion(name, start_date, end_date, discount)
     VALUES (inputPromotionName, inputStartDate, inputEndDate, inputDiscount);
@@ -284,7 +285,7 @@ BEGIN
     -- Update all prices in product table
     UPDATE product
     SET price = price - (price * inputDiscount);
-
+    END IF;
     -- Return success message
     SELECT 'successfully launched promotion' AS message;
 END$$
